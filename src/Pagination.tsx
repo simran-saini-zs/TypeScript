b56f4ba37b0620688d9d22e39c7f00a6ab56a321
Raramuri <raramuri@Raramuris-MacBook-Pro.local>
2025-01-31 16:40:21 +0530

    Worked on comments

--- src/Pagination.tsx
+++ src/Pagination.tsx
@@ -26,7 +26,6 @@ let idx:number = (pageNo-1)*5;
                 {
                     return (
                         <div className='content' key = {i}>
-                        {/* id: {data[idx]["id"]} */}
                         <p className='txt'>{data[idx]["title"]}</p>
                         <img src={data[idx++]["images"][0]} alt="hii" className='img'/>
                         </div>
@@ -43,8 +42,8 @@ let idx:number = (pageNo-1)*5;
             </div>
             <div className="buttons">
                 
-                <button className={pageNo==1?'btn disable':'btn'} onClick={()=>{setPageNo(1)}}>≪</button>
-                <button className={pageNo==1?'btn disable':'btn'} onClick={()=>{setPageNo((prev)=>prev == 1?1:prev-1)}}>{"<"}</button>
+                <button className={pageNo==1?'btn disable':'btn'} onClick={()=>{setPageNo(max_pageNo)}}>≪</button>
+                <button className={pageNo==1?'btn disable':'btn'} onClick={()=>{setPageNo((prev)=>prev == max_pageNo?1:prev-1)}}>{"<"}</button>
                 
                 {pageNo-2 == max_pageNo-2 ?<button className='btn btn-2' onClick={()=>{setPageNo((prev)=>prev-2)}}>{pageNo-2}</button>:null }
                 {pageNo-1 >= max_pageNo-2? <button className='btn btn-2' onClick={()=>{setPageNo((prev)=>prev-1)}}>{pageNo-1}</button>:null}
@@ -53,8 +52,8 @@ let idx:number = (pageNo-1)*5;
                 {pageNo+2 <= max_pageNo?  <button className='btn btn-2' onClick={()=>{setPageNo((prev)=>prev+2)}}>{pageNo+2}</button>:null}
 
                 {pageNo+2 < max_pageNo?<button className='btn btn-2'>.....</button>:null}
-                <button className={pageNo==max_pageNo?'btn disable':'btn'} onClick={()=>{setPageNo((prev)=>prev==max_pageNo?max_pageNo:prev+1)}}>{">"}</button>
-                <button className={pageNo==max_pageNo?'btn disable':'btn'} onClick={()=>{setPageNo(max_pageNo)}}>≫</button>
+                <button className={pageNo==max_pageNo?'btn disable':'btn'} onClick={()=>{setPageNo((prev)=>prev==max_pageNo?1:prev+1)}}>{">"}</button>
+                <button className={pageNo==max_pageNo?'btn disable':'btn'} onClick={()=>{setPageNo(1)}}>≫</button>
             </div>
         </div>
         </>
