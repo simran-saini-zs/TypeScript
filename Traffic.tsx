cd732f90d386337456075f424c95b682705e91b7
Raramuri <raramuri@Raramuris-MacBook-Pro.local>
2025-02-04 17:05:25 +0530

    Working

---
+++ Traffic.tsx
@@ -0,0 +1,13 @@
+import './Traffic.css'
+
+function Traffic()
+{
+    return (
+        <>
+        <div className='light red'></div>
+        <div className='light yellow'></div>
+        <div className='light green'></div>
+        </>
+    )
+}
+export default Traffic
